602f9585579b53d619fe8f0eb4158b5964010cef
xeniah <xeniah@gmail.com>
2020-10-22 08:00:59 -0700

    Removed the incorrectly named chapters

--- Chapter-2/sql-scripts/CreateEmailRecipientsTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[EmailRecipient](
-	FirstName varchar(100) NULL,
-	LastName varchar(100) NULL,
-	EmailAddress varchar(100) NULL
-)
-
-INSERT INTO [dbo].[EmailRecipient](FirstName, LastName, EmailAddress)
-VALUES (<First-Name>, <Last-Name>, <Email-Address>);
