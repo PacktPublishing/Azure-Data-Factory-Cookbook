fccbcdb29576ef038e5e175ce4138256aaa7f4bd
dfoshin <68492342+dfoshin@users.noreply.github.com>
2020-10-27 11:49:58 +0300

    Delete Chapter3-Using SQL on-demand.sql

--- Chapter3-Using SQL on-demand.sql
+++
@@ -1,9 +0,0 @@
--- Using SQL on-demand to connect to Parquet file
-
-SELECT
-    TOP 100 *
-FROM
-    OPENROWSET(
-        BULK 'https://adfcookbookch3adls.dfs.core.windows.net/flightscontainer/dbo.Routes.parquet',
-        FORMAT='PARQUET'
-    ) AS [r];
