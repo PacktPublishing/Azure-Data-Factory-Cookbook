8c1e9d1f624bc34ec75fe96a352227fd7ec59559
dfoshin <68492342+dfoshin@users.noreply.github.com>
2020-10-27 11:49:17 +0300

    Delete Chapter3-Create user.sql

--- Chapter3-Create user.sql
+++
@@ -1,6 +0,0 @@
--- Creating user for loading data
-
-CREATE LOGIN adfuser WITH PASSWORD = 'ADF3password';
-CREATE USER adfuser FOR LOGIN adfuser;
-GRANT CONTROL ON DATABASE::[adfcookbookch1devsqldb] to adfuser;
-EXEC sp_addrolemember 'staticrc10', 'adfuser';
